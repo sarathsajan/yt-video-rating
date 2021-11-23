1c4215a760fcbc445f256156d0e45006268f7851
Sarath Sajan <sarathsajan1998@gmail.com>
2021-11-23 21:15:02 +0530

    FUBAR

--- get_metrics.js
+++ get_metrics.js
@@ -1,9 +1,5 @@
 let views_metric = "0";
-let likes_metric = "0";
-let comments_metric = "0";
 
-views_metric = document.getElementById("view-count style-scope ytd-video-view-count-renderer");
+views_metric = document.getElementsByClassName("view-count").innerText;
 console.log("Extension running");
 console.log(views_metric);
-console.log(likes_metric);
-console.log(comments_metric);
