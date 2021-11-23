6888bab3fe7d157051bbed265b487fb80fc7938f
Sarath Sajan <sarathsajan1998@gmail.com>
2021-11-23 20:00:20 +0530

    v0.2 created and registered the get_metrics script

---
+++ get_metrics.js
@@ -0,0 +1,8 @@
+let views_metric = "0";
+let likes_metric = "0";
+let comments_metric = "0";
+
+console.log("Extension started");
+console.log(views_metric);
+console.log(likes_metric);
+console.log(comments_metric);
