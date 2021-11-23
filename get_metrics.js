bd757f92d24e824736d24aa75a3ec77dc4a2fba2
Sarath Sajan <sarathsajan1998@gmail.com>
2021-11-23 20:51:40 +0530

    deleted unwanted files

--- get_metrics.js
+++ get_metrics.js
@@ -2,9 +2,8 @@ let views_metric = "0";
 let likes_metric = "0";
 let comments_metric = "0";
 
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extension started");
-    console.log(views_metric);
-    console.log(likes_metric);
-    console.log(comments_metric);
-})
+views_metric = document.getElementById("view-count style-scope ytd-video-view-count-renderer");
+console.log("Extension running");
+console.log(views_metric);
+console.log(likes_metric);
+console.log(comments_metric);
