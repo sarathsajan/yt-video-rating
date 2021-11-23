d4e0cc80f7f03500e0d14a11a1a1b30f1b8ca885
Sarath Sajan <sarathsajan1998@gmail.com>
2021-11-23 20:18:54 +0530

    v0.3 added icons and started work on interface

--- get_metrics.js
+++ get_metrics.js
@@ -2,7 +2,9 @@ let views_metric = "0";
 let likes_metric = "0";
 let comments_metric = "0";
 
-console.log("Extension started");
-console.log(views_metric);
-console.log(likes_metric);
-console.log(comments_metric);
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extension started");
+    console.log(views_metric);
+    console.log(likes_metric);
+    console.log(comments_metric);
+})
