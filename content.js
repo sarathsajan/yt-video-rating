fcaa3ae89ae89779fbe2a72a162465a47f9e4af4
Sarath Sajan <sarathsajan1998@gmail.com>
2021-11-28 00:00:17 +0530

    bookmarklets done

--- content.js
+++ content.js
@@ -1,12 +1,13 @@
 console.log("Hello World from Chrome Extension");
 
-let video_views = document.getElementsByClassName("view-count style-scope ytd-video-view-count-renderer")[0].innerText.slice(0, -6);
-console.log(video_views);
+// let video_views_raw = document.getElementsByClassName("view-count style-scope ytd-video-view-count-renderer")[0].innerText;
 
-let video_likes = document.getElementsByClassName("style-scope ytd-toggle-button-renderer style-text")[1].innerText.slice(0, -1);
-let video_likes_multiplier = document.getElementsByClassName("style-scope ytd-toggle-button-renderer style-text")[1].innerText.slice(-1);
-console.log(video_likes);
-console.log(video_likes_multiplier);
+video_views_raw = '65,631 views'
+console.log(video_views_raw);
+
+// let video_likes_raw = document.getElementsByClassName("style-scope ytd-toggle-button-renderer style-text")[1].innerText;
+video_likes_raw = '1.3K'
+console.log(video_likes_raw);
 
 // console.log(video_views);
 // console.log(video_likes);
